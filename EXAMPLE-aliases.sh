ec39bd5912bf504411b4e61099495b20b2010a70
Andy <37462713+andym1125@users.noreply.github.com>
2023-03-02 11:59:47 -0600

    Aliases added for dev experience

---
+++ EXAMPLE-aliases.sh
@@ -0,0 +1,15 @@
+# Copy to aliases.sh in this directory
+# Add aliases.sh to your profile (.bashrc or .zshrc) to automatically get these aliases
+# Be sure to change the directory path if the project is in a different location.
+
+alias rf="cd ~/Documents/workshop/eco-bit-by-bit"
+alias bf="cd ~/Documents/workshop/eco-bit-by-bit/server"
+alias ff="cd ~/Documents/workshop/eco-bit-by-bit/client"
+alias sf="cd ~/Documents/workshop/eco-bit-by-bit/scraper"
+
+alias goto-root="rf"
+alias goto-backend="bf"
+alias goto-frontend="ff"
+alias goto-scraper="sf"
+
+echo Aliases set up
