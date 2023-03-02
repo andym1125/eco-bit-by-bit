2477ad8d3f2bba7ebdf7cc95b72838da1b7f6d1c
Andy <37462713+andym1125@users.noreply.github.com>
2023-03-02 12:24:34 -0600

    POC scraper

--- packages/scraper/src/index.ts
+++ packages/scraper/src/index.ts
@@ -1,1 +1,12 @@
-console.log("test")
+import puppeteer from 'puppeteer'
+
+async function start() {
+    const browser = await puppeteer.launch()
+    const page = await browser.newPage()
+    await page.goto("https://www.amazon.com/Skechers-Expected-Avillo-Relaxed-Fit-Loafer/dp/B00P6LHCAO/")
+
+    console.log(await page.$eval('#title', (el) => el.innerHTML))
+
+    await browser.close()
+}
+start()
