28ac4df17375a1e293ebd85de0bdcee229198b09
Andy <37462713+andym1125@users.noreply.github.com>
2023-03-02 12:03:53 -0600

    fix aliases

--- EXAMPLE-aliases.sh
+++ EXAMPLE-aliases.sh
@@ -1,11 +1,9 @@
-# Copy to aliases.sh in this directory
-# Add aliases.sh to your profile (.bashrc or .zshrc) to automatically get these aliases
-# Be sure to change the directory path if the project is in a different location.
+# Copy this code to your profile (.bashrc or .zshrc) to automatically get these aliases
 
 alias rf="cd ~/Documents/workshop/eco-bit-by-bit"
-alias bf="cd ~/Documents/workshop/eco-bit-by-bit/server"
-alias ff="cd ~/Documents/workshop/eco-bit-by-bit/client"
-alias sf="cd ~/Documents/workshop/eco-bit-by-bit/scraper"
+alias bf="cd ~/Documents/workshop/eco-bit-by-bit/packages/server"
+alias ff="cd ~/Documents/workshop/eco-bit-by-bit/packages/client"
+alias sf="cd ~/Documents/workshop/eco-bit-by-bit/packages/scraper"
 
 alias goto-root="rf"
 alias goto-backend="bf"
