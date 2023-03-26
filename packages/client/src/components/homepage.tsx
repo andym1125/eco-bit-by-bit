8c5e280811f71975c1569188a62ed0853bdfdcb9
Aryan-Patel5475 <118410521+Aryan-Patel5475@users.noreply.github.com>
2023-03-25 19:17:06 -0500

    Update homepage.tsx

--- packages/client/src/components/homepage.tsx
+++ packages/client/src/components/homepage.tsx
@@ -5,34 +5,18 @@ let Logo = require('./logo.png');
 
 function Homepage() {
     return (
-        <div className="homePage">
-            <h1
-                style={{
-                    backgroundColor: "#6096B4",
-                    width: "100%",
-                    height: "100px",
-                }}
-            >
-                <img
-                    style={{ width: 100, height: 100 }}
-                    src={Logo}
-                    alt="React Logo"
-                />
-            </h1>
-            <p
-                style={{
-                    backgroundColor: "#EEE9DA",
-                    paddingTop: "300px",
-                    fontSize: 50,
-                    width: "100%",
-                    height: "100vh",
-                }}
-            >
-                {"\n"}
-                Website under construction
-            </p>
-        </div>
-    );
+  <div className="homePage">
+    <h1>
+      <img
+        style={{ width: 100, height: 100 }}
+        src={Logo}
+        alt="React Logo"
+      />
+    </h1>
+    <p>{"\n"}Website under construction</p>
+  </div>
+);
+
 }
 
 export default Homepage;
