f0d90355b11f7028cbe3236903c1e188a490341e
Andy <37462713+andym1125@users.noreply.github.com>
2023-03-28 12:03:42 -0500

    Hotfix: algo.ts

--- packages/server/algo.ts
+++ packages/server/algo.ts
@@ -1,6 +1,6 @@
-let Sustainable_Clothing_Materials: string[] = ['Hemp', 'Organic Cotton', 'Organic Linen', 'Lyocell', 'Enconyl', 'Cork', 'Pinatex']; //Some common sustainable clothing materials to compare products with
+let SUSTAINABLE_CLOTHING_MATERIALS: string[] = ['Hemp', 'Organic Cotton', 'Organic Linen', 'Lyocell', 'Enconyl', 'Cork', 'Pinatex']; // Some common sustainable clothing materials to compare products with
 
-//Check if product materials matches any of the sustainable materials in above array
-function Contains_Sustainable_Materials(product_material string){
-    return Sustainable_Clothing_Materials.find(element => element == product_material);
+// Check if product materials matches any of the sustainable materials in above array
+function Contains_Sustainable_Materials(productMaterial: string){
+    return SUSTAINABLE_CLOTHING_MATERIALS.find(element => element === productMaterial);
 }
