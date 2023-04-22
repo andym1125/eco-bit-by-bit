9563b9f5100b837ab37c60384f9d834e7f3d15fd
Aryan-Patel5475 <118410521+Aryan-Patel5475@users.noreply.github.com>
2023-04-22 15:44:38 -0500

    fetching data again from compare since I couldn't work out how to export data, and showing data in a graph form.

--- packages/client/src/components/compare/Compare.tsx
+++ packages/client/src/components/compare/Compare.tsx
@@ -1,15 +1,134 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React, {useEffect, useRef, useState} from 'react';
+import {useInputValues} from "../textbox/InputValuesContext";
+import Chart from 'chart.js/auto';
+import './Compare.css'
 
-function Quiz() {
-    return (
-        <div className="Compare-app">
-            <header className="Compare-header">
-                <p> Compare Page </p>
-                <Link to="/"> go back </Link>
-            </header>
-        </div>
-    );
+interface ScoreResponseBody {
+    score: number,
+    breakdown: {
+        water: number,
+        carbon: number,
+        esg: 'CCC' | 'B' | 'BB' | 'BBB' | 'A' | 'AA' | 'AAA',
+        bio: number,
+        recycle: number,
+        durable: number,
+        ctx: string
+    },
+    expl: string,
+    err: string,
+    reli_expl: string,
+    reliability: number,
 }
 
-export default Quiz;
+function Compare() {
+    const {inputValues} = useInputValues();
+    const [allScores, setAllScores] = useState<ScoreResponseBody[]>([]);
+    const initialFetchComplete = useRef(false);
+
+    const nameArray = inputValues.map((inputUrl) => {
+        const urlParts = inputUrl.split("/");
+        return urlParts[urlParts.indexOf("dp") - 1];
+    });
+
+    const scoresArray = allScores.map((score) => score.score);
+    const reliabilityArray = allScores.map((score) => score.reliability);
+    const waterArray = allScores.map((score) => score.breakdown.water);
+    const carbonArray: number[] = allScores.map((score: ScoreResponseBody) => score.breakdown.carbon);
+    const esgArray = allScores.map((score) => score.breakdown.esg);
+    const bioArray = allScores.map((score) => score.breakdown.bio);
+    const recycleArray = allScores.map((score) => score.breakdown.recycle);
+    const durableArray = allScores.map((score) => score.breakdown.durable);
+
+    const chartRef = useRef<Chart | null>(null);
+
+    useEffect(() => {
+        if (!initialFetchComplete.current) {
+            inputValues.forEach((inputUrl) => {
+                fetchScoreData(inputUrl);
+            });
+            initialFetchComplete.current = true;
+        }
+    }, [inputValues]);
+
+    const fetchData = async (url: string) => {
+        try {
+            const response = await fetch(url);
+            if (response.ok) {
+                const data: ScoreResponseBody = await response.json();
+                console.log("Fetched data:", data);
+                setAllScores((prevScores) => [...prevScores, data]);
+            } else {
+                console.error("Error fetching data. Status:", response.status);
+            }
+        } catch (error) {
+            console.error("Error fetching data:", error);
+        }
+    };
+
+    const fetchScoreData = (inputUrl: string) => {
+        const productId = inputUrl.split("/").slice(3).join("/");
+        fetchData(`http://localhost:3001/score/${productId}`);
+    };
+
+    useEffect(() => {
+        const data = {
+            labels: nameArray,
+            datasets: [
+                {
+                    label: 'Water',
+                    data: waterArray,
+                    backgroundColor: 'rgba(54, 162, 235, 0.5)'
+                },
+                {
+                    label: 'Carbon',
+                    data: carbonArray,
+                    backgroundColor: 'rgba(255, 206, 86, 0.5)'
+                },
+                {
+                    label: 'Bio',
+                    data: bioArray,
+                    backgroundColor: 'rgba(153, 102, 255, 0.5)'
+                },
+                {
+                    label: 'Recycle',
+                    data: recycleArray,
+                    backgroundColor: 'rgba(255, 159, 64, 0.5)'
+                },
+                {
+                    label: 'Durable',
+                    data: durableArray,
+                    backgroundColor: 'rgba(255, 99, 132, 0.5)'
+                }
+            ]
+        };
+
+        const canvas = document.getElementById('myChart') as HTMLCanvasElement;
+
+        if (chartRef.current) {
+            chartRef.current.destroy();
+        }
+
+        chartRef.current = new Chart(canvas, {
+            type: 'bar',
+            data: data,
+            options: {
+                scales: {
+                    x: {
+                        stacked: true
+                    },
+                    y: {
+                        stacked: true
+                    }
+                }
+            }
+        });
+    }, [allScores]);
+
+    return (
+        <div className = 'chart'>
+            <canvas id="myChart"></canvas>
+        </div>
+    )
+}
+
+export default Compare;
